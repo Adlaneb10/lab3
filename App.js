154e20afb85e25f0ed179446d0bb444f6742a000
Adlaneb10 <55700960+Adlaneb10@users.noreply.github.com>
2022-10-17 11:30:34 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,27 +1,87 @@
 
-import './App.css';
+import { useState } from "react";
+import './App.css'
+
+function EntryForm({ addEntryToPhoneBook }) {
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addEntryToPhoneBook({ name, phoneNumber, email });
+  };
+  return (
+    <div className="bodyTop">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="text"
+          name="number"
+          id="number"
+          placeholder="Mobile Number"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+        />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit" onSubmit={handleSubmit}>
+          submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+
 
 function App() {
+  const [entries, setEntries] = useState([]);
+
+  const addEntryToPhoneBook = (entry) => {
+    setEntries(
+      [...entries, entry].sort((a, b) =>
+        a.lastName.toLowerCase() > b.lastName.toLowerCase() ? 1 : -1
+      )
+    );
+  };
+
   return (
     <div className="App">
-        <h1>Phone Directory</h1>
-        <div className='bodyTop'>
-          <div className='inputs'>
-              <input type="text" placeholder="Contact Name" id="contact"/>
-              <input type="number" placeholder="Mobile Number" id="number"/>
-              <input type="email" placeholder="Email" id="email"/>
-              <button>Add Contact</button>
-          </div>
-        </div>
-        <div className='directory'>
+      <EntryForm addEntryToPhoneBook={addEntryToPhoneBook} />
+      <div className='directory'>
           <div className='upperDir'>
             <p>Search Contact by Mobile No:</p>
             <input type="text" id="search"></input>
           </div>
           <div className='lowerDir'>
-            <h1>Contacts Directory</h1>
-            <table>
-              <tr>Name</tr>
+            <h1>Contacts Summary</h1>
+            <table className="tableDiv">
+              <tr>
+                <th>Name</th>
+                <th>Mobile</th>
+                <th>Email</th>
+              </tr>
+              {entries.map((entry)=> (
+                <tr>
+                  <th>{entry.name}</th>
+                  <th>{entry.phoneNumber}</th>
+                  <th>{entry.email}</th>
+              </tr>
+              ))}
+
             </table>
           </div>
         </div>
